7b96d03cf0729a1ba38c876724e3a04a54031211
Hiroyuki Ito <hageyahhoo@gmail.com>
2018-12-25 12:17:16 +0900

    Added lookup(), update(), and get_bucket() & renamed hash_string() and make_hashtable().

--- test/test_hash.py
+++ test/test_hash.py
@@ -7,22 +7,66 @@ class TestHash(unittest.TestCase):
         self.func = Hash()
 
 
-    def test_hash_function_emptyString(self):
-        self.assertEqual(self.func.hash_function('', 12), 0)
+    def test_lookup_Found(self):
+        hashtable = self.func.make_hashtable(3)
+        self.func.update(hashtable, 'foo', 1)
+        self.func.update(hashtable, 'bar', 2)
+        self.func.update(hashtable, 'baz', 3)
 
-    def test_hash_function_normal(self):
-        self.assertEqual(self.func.hash_function('a', 12), 1)
-        self.assertEqual(self.func.hash_function('b', 12), 2)
-        self.assertEqual(self.func.hash_function('c', 12), 3)
-        self.assertEqual(self.func.hash_function('LIJ', 12), 7)
+        self.assertEqual(self.func.lookup(hashtable, 'bar'), 2)
+
+    def test_lookup_NotFound(self):
+        hashtable = self.func.make_hashtable(3)
+        self.func.update(hashtable, 'foo', 1)
+        self.func.update(hashtable, 'bar', 2)
+        self.func.update(hashtable, 'baz', 3)
+
+        self.assertIsNone(self.func.lookup(hashtable, 'are'))
 
 
-    def test_make_hash_table(self):
-        hashtable = self.func.make_hash_table(3)
+    def test_update_Add(self):
+        hashtable = self.func.make_hashtable(3)
+        self.func.update(hashtable, 'foo', 1)
+
+        self.assertEqual(len(hashtable[0]), 1)
+        self.assertEqual(hashtable[0][0], ['foo', 1])
+        self.assertEqual(len(hashtable[1]), 0)
+        self.assertEqual(len(hashtable[2]), 0)
+
+    def test_update_Update(self):
+        hashtable = self.func.make_hashtable(3)
+        self.func.update(hashtable, 'foo', 1)
+        self.func.update(hashtable, 'foo', 2)
+        self.assertEqual(len(hashtable[0]), 1)
+        self.assertEqual(hashtable[0][0], ['foo', 1])
+        self.assertEqual(len(hashtable[1]), 0)
+        self.assertEqual(len(hashtable[2]), 0)
+
+
+    def test_get_bucket(self):
+        hashtable = [[1], [2], [3]]
+        # ord('a') = 97, buckets_size = 3, 97 % 3 = 1
+        self.assertEqual(self.func.get_bucket(hashtable, 'a'), hashtable[1])
+        self.assertEqual(self.func.get_bucket(hashtable, 'a'), [2])
+
+
+    def test_hash_string_EmptyString(self):
+        self.assertEqual(self.func.hash_string('', 12), 0)
+
+    def test_hash_string_Normal(self):
+        self.assertEqual(self.func.hash_string('a', 12), 1)
+        self.assertEqual(self.func.hash_string('b', 12), 2)
+        self.assertEqual(self.func.hash_string('c', 12), 3)
+        self.assertEqual(self.func.hash_string('LIJ', 12), 7)
+
+
+    def test_make_hashtable(self):
+        hashtable = self.func.make_hashtable(3)
 
         self.assertEqual(len(hashtable), 3)
         self.assertEqual(hashtable, [[], [], []])
 
+        # Don't use [[]] * n
         hashtable[1].append('url')
         self.assertEqual(hashtable, [[], ['url'], []])
 
--- web_crawler/hash.py
+++ web_crawler/hash.py
@@ -1,13 +1,39 @@
 class Hash:
 
-    def hash_function(self, keyword, buckets):
-        hash_value = 0
-        for char in keyword:
-            hash_value = (hash_value + ord(char)) % buckets
-        return hash_value
+    def lookup(self, hashtable, keyword):
+        # TODO Refactor
+        bucket = self.get_bucket(hashtable, keyword)
+        for k, v in bucket:
+            if k == keyword:
+                return v
+        return None
 
-    def make_hash_table(self, buckets):
+
+    def update(self, hashtable, keyword, value):
+        # TODO Refactor
+        bucket = self.get_bucket(hashtable, keyword)
+        for k, v in bucket:
+            if k == keyword:
+                # Update
+                v = value
+                return hashtable
+        bucket.append([keyword, value])
+        return hashtable
+
+
+    def get_bucket(self, hashtable, keyword):
+        return hashtable[self.hash_string(keyword, len(hashtable))]
+
+
+    def hash_string(self, keyword, buckets_size):
+        hash_string = 0
+        for char in keyword:
+            hash_string = (hash_string + ord(char)) % buckets_size
+        return hash_string
+
+    def make_hashtable(self, buckets_size):
+        # Don't use [[]] * n
         hashtable = []
-        for unused in range(0, buckets):
+        for unused in range(0, buckets_size):
             hashtable.append([])
         return hashtable
